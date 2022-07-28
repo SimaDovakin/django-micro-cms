6f8c6c541ec1333f816b95060b5993a411ec803d
SimaDovakin <simadovakin999@gmail.com>
2022-07-28 11:51:48 +0300

    Created models for pages app.

--- micro_cms/pages/models.py
+++ micro_cms/pages/models.py
@@ -1,3 +1,36 @@
+from django.contrib.sites.models import Site
+from django.contrib.sites.managers import CurrentSiteManager
+from django.contrib.sites.shortcuts import get_current_site
 from django.db import models
 
-# Create your models here.
+
+def _save_image(instance, filename):
+    return f"section_image/{get_current_site().id}_{instance.pk}_{filename}"
+
+
+class WebPage(models.Model):
+    title = models.CharField(max_length=64)
+    site = models.OneToOneField(Site, on_delete=models.CASCADE)
+
+    # Model managers
+    objects = models.Manager()
+    on_site = CurrentSiteManager()
+
+    def __str__(self):
+        return self.title
+
+
+class PageSection(models.Model):
+    title = models.CharField(max_length=64)
+    image = models.ImageField(
+        upload_to=_save_image,
+        blank=True
+    )
+    html_content = models.TextField()
+    order = models.PositiveSmallIntegerField(default=0)
+    page = models.ForeignKey(WebPage, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
