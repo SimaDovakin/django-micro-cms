8934f2ef667efaee19900a4bd9b0732973c704e5
SimaDovakin <simadovakin999@gmail.com>
2022-07-28 10:36:43 +0300

    Enabled Django sites framework.

--- micro_cms/micro_cms/settings.py
+++ micro_cms/micro_cms/settings.py
@@ -22,6 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-lc7o)c)e^#v%3h2-)y4-$hn$(8m52qbq8fn)%j^l*3-auyfji*'
 
+SITE_ID = 1
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites'
 ]
 
 MIDDLEWARE = [
