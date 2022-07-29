9936126bb51583f00c2371bc1c6676806e4a072c
SimaDovakin <simadovakin999@gmail.com>
2022-07-29 13:32:36 +0300

    Added main page and manually tested it.

--- micro_cms/pages/views.py
+++ micro_cms/pages/views.py
@@ -1,3 +1,27 @@
+from django.contrib.sites.shortcuts import get_current_site
 from django.shortcuts import render
 
-# Create your views here.
+from .models import WebPage, PageSection
+from products.models import Device
+
+
+def index(request):
+    current_site = get_current_site(request)
+
+    page = WebPage.objects.get(site=current_site)
+    page_sections = PageSection.objects.filter(
+        page=page,
+        is_active=True
+    ).order_by('order')
+    devices = Device.objects.filter(
+        page=page
+    ).select_related('vendor')
+
+    context = {
+        'page': page,
+        'page_sections': page_sections,
+        'devices': devices
+    }
+
+    return render(request, 'pages/index.html', context)
+
